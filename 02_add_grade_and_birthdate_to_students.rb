8200b5feedfc3207f1290bb60738014ec991c154
Alex Alanya <alexandro.alanya@gmail.com>
2019-06-17 00:26:32 +0000

    Automatically backed up by Learn

--- 02_add_grade_and_birthdate_to_students.rb
+++ 02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  add_column :grade, :birthdate do |t|
+  def change
+    add_column :grade, :birthdate do |t|
     t.integer :grade
     t.string :birthdate
-  end 
-end
-
+    end 
+  end
 end
